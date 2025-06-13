bb9784cdf66ec4ee683b29c3048150c1e5c3d5e8
ADJADI <mradjadi.a@gmail.com>
2025-06-13 13:47:52 +0200

    ci: Adding routes and add the image url in the env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,4 +2,6 @@ export const config = {
   apiKey: import.meta.env.VITE_TMDB_API_KEY,
   apiToken: import.meta.env.VITE_TMDB_TOKEN_API,
   apiBaseUrl: import.meta.env.VITE_TMDB_API_URL,
+  apiImageUrl : import.meta.env.VITE_TMDB_IMAGE_URL,
+
 };
---
+++ src/config/route.js
@@ -0,0 +1,8 @@
+export const ROUTES = Object.freeze({
+  HOME: "/",
+  AUTH: "/auth",
+  MOREDETAIL: "/moredetail/:id",
+  SIGNIN: "/auth/signin/",
+  SIGNUP: "/auth/signup/",
+  ERROR404: "*",
+});
