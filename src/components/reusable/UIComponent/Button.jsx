4b768a6379367b08036f456aa3e76e74abc0bcaf
ADJADI <mradjadi.a@gmail.com>
2025-06-13 13:48:37 +0200

    style: update button style

--- src/components/reusable/UIComponent/Button.jsx
+++ src/components/reusable/UIComponent/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button({ color, size, logo, justify, info }) {
+export default function Button({ style, info, logo }) {
 
     return (
-        <button style={{ justifyContent: justify, width: size, height: size, backgroundColor: color }} className='flex items-center' >
+        <button className={`flex items-center ${style}`} >
             <div><img src={logo} alt="" /></div>
             <span>{info}</span>
         </button >
